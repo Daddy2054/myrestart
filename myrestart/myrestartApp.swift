092a59281bf43d8187f7c1e0f3888a38a0a162ea
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-01 23:19:15 +0100

    Initial Commit

---
+++ myrestart/myrestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  myrestartApp.swift
+//  myrestart
+//
+//  Created by Jean on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct myrestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
