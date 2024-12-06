28c6fbb62051d52d9cb1cd1ab493f821e5f4543c
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-12-06 19:37:31 +0100

    Chapter 4 - Drag Gesture

--- myrestart/Screens/OnboardingView.swift
+++ myrestart/Screens/OnboardingView.swift
@@ -11,6 +11,9 @@ struct OnboardingView: View {
     //    MARK: - PROPERTY
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    
     //    MARK: - BODY
     var body: some View {
         ZStack {
@@ -66,7 +69,7 @@ how much love we put into giving.
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                         
                     }
@@ -83,13 +86,31 @@ how much love we put into giving.
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(DragGesture()
+                            .onChanged{gesture in
+                                if gesture.translation
+                                    .width > 0 &&
+                                    buttonOffset
+                                     <= buttonWidth - 80
+                                {
+                                    buttonOffset = gesture.translation.width
+                                }
+                            }
+                            .onEnded{ _ in
+                                if buttonOffset > buttonWidth / 2 {
+                                    buttonOffset = buttonWidth - 80
+                                    isOnboardingViewActive = false
+                                } else {
+                                    buttonOffset = 0
+                                }
+                         
+                            }
+                        ) //: GESTURE
                         Spacer()
                     } //: HSTACK
                 } //: FOOTER
-                .frame(height: 80,alignment: .center)
+                .frame(width: buttonWidth, height: 80,alignment: .center)
                 .padding()
                 
             }//: VSTACK
@@ -97,6 +118,7 @@ how much love we put into giving.
     }
 }
 
+//MARK: - PREVIEW
 #Preview {
     OnboardingView()
 }
